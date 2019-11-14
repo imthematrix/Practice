187011dbc4cc8d65eb495bda762b0190c42b07da
manish kumar <manish.kumar7@aig.com>
2019-11-14 14:47:21 +0530

    added necessary comments

--- PYTHON/picking-numbers.py
+++ PYTHON/picking-numbers.py
@@ -8,6 +8,14 @@ def pickingNumbers(a):
             temp=d[i]
             d[i]=temp+1
     # print('d =', d)
+    # working with dictionary is really awesome.
+    # this problem are unbreakable until I got a hint to use dictionary and
+    # the idea that if each element will have a difference of <=1, then 
+    # that particulat array will have only two distinct elements
+    # one which will be having a difference of 0 other which will be having a difference of 1
+
+    # if we were to talk about a difference <=2 the total distinct elements would be 3
+    # one with 0 difference, 2nd with 1 difference and third with 2 difference. Remember this.
     largest=0
     
     for i in d.keys():
