2ac543efed8ea82e32e46e9ffb379c3302c60034
imthematrix <imthematrix@users.noreply.github.com>
2019-11-20 21:21:55 +0530

    dll hell study

---
+++ DOTNET/DLLHELL/App2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ClassLibrary;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace App2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sample.Message2());
+            Console.ReadKey();
+        }
+    }
+}
