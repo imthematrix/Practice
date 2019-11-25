d2948c9fd417112e56468d9849d9faa636cae6f2
manish kumar <manish.kumar7@aig.com>
2019-11-25 19:47:27 +0530

    11/25 office last push

---
+++ DOTNET/ArrayInCSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayInCSharp
+{
+    class Program
+    {
+        /// <summary>
+    /// this is the main method, the words describing it are coming from the XML comments
+    /// </summary>
+    /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int[] num = new int[3];
+            Console.WriteLine(num.Length);
+            Console.ReadKey();
+        }
+    }
+}
