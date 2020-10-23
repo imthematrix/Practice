a4b9daaa4f786c120fca9383e3370a44c27c7970
Manish Kumar <manish.aec2012@gmail.com>
2020-10-23 11:14:20 +0530

    send puch mule4

---
+++ Mulesoft/workingwithdatabases/sql.sql
@@ -0,0 +1,21 @@
+/*show databases;*/
+use sakila;
+SELECT `film`.`film_id`,
+    `film`.`title`,
+    `film`.`description`,
+    `film`.`release_year`,
+    `film`.`language_id`,
+    `film`.`original_language_id`,
+    `film`.`rental_duration`,
+    `film`.`rental_rate`,
+    `film`.`length`,
+    `film`.`replacement_cost`,
+    `film`.`rating`,
+    `film`.`special_features`,
+    `film`.`last_update`
+FROM `sakila`.`film`
+where `film`.`rating` like 'PG%';
+
+
+
+
