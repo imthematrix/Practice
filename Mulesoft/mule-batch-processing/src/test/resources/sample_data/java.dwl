675d864b1fa701ce84f7950e74a8a0b9b75fdf48
Manish Kumar <manish.aec2012@gmail.com>
2020-11-05 11:26:06 +0530

    studied till mule batch

---
+++ Mulesoft/mule-batch-processing/src/test/resources/sample_data/java.dwl
@@ -0,0 +1,18 @@
+
+%dw 2.0
+output application/java 
+---
+{
+  Transaction_date : "fugiat",
+  Product : "nostrum",
+  Price : "53.95",
+  Payment_Type : "quasi",
+  Name : "Ammie",
+  City : "New York",
+  State : "aute",
+  Country : "Guyana",
+  Account_Created : "dolorem",
+  Last_Login : "ratione",
+  Latitude : "Nemo",
+  Longitude : "laboris",
+}
--- Mulesoft/watermarkingdatabases/example.sql
+++ Mulesoft/watermarkingdatabases/example.sql
@@ -1,10 +1,4 @@
-select film_id, title, description from film_text order by film_id desc
+SELECT `details`.`name`,
+    `details`.`city`
+FROM `sakila`.`details`;
 
-INSERT INTO `sakila`.`film_text`
-(`film_id`,
-`title`,
-`description`)
-VALUES
-(1006,
-'test movie after the program is running',
-'this is a test during handmade watermarking another one, yes');
