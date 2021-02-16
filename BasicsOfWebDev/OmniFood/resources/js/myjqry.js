9532516e22eb8bd053fe2581d195078cca81ce05
Manish Kumar <manish.aec2012@gmail.com>
2021-02-16 14:34:21 +0530

    navigation completed

--- BasicsOfWebDev/OmniFood/resources/js/myjqry.js
+++ BasicsOfWebDev/OmniFood/resources/js/myjqry.js
@@ -10,6 +10,7 @@ $(document).ready(function(){
     })
     */
 
+    //identify the direction of scroll and add the sticky class to nav element
     $('.js--section-features').waypoint(
         function(direction){
             if(direction =='down' ){
@@ -21,6 +22,52 @@ $(document).ready(function(){
         },
         // {offset: '60px;'}
         {offset: '10%'}
-        );
+    );
+
+    //scroll to plan section
+    $('.js--scroll-to-plans').click(function(){
+        $('html, body').animate({scrollTop:$('.js--section-plans').offset().top}, 1200);
+    });
+
+    //scroll to features section
+    $('.js--scroll-to-features').click(function(){
+        $('html, body').animate({scrollTop:$('.js--section-features').offset().top}, 1200);
+    });
+
+  //animate the navigation to different sections -- smooth scrolling
+    $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')    &&   location.hostname == this.hostname
+        ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1200, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
 
 });
